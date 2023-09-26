ffab910ae3b51aa64ebdb06cd45e0ed6fcfd1650
ukant07 <43910600+ukant07@users.noreply.github.com>
2023-09-26 18:59:36 +0530

    main5.js
    Storing in localStorage

--- main5.js
+++ main5.js
@@ -1,1 +1,10 @@
+  document.getElementById('my-form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
 
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+
+  localStorage.setItem('userName', name);
+  localStorage.setItem('userEmail', email);
+    
+  document.getElementById('my-form').reset();
