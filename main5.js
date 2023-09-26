36442c4ec7cd8608b66babaeb32342cd5941f7fe
ukant07 <43910600+ukant07@users.noreply.github.com>
2023-09-26 19:42:39 +0530

    main5.js updated

--- main5.js
+++ main5.js
@@ -1,10 +1,36 @@
+//  document.getElementById('my-form').addEventListener('submit', function(event) {
+//  event.preventDefault(); 
+
+//  const name = document.getElementById('name').value;
+//  const email = document.getElementById('email').value;
+
+//  localStorage.setItem('userName', name);
+//  localStorage.setItem('userEmail', email);
+    
+//  document.getElementById('my-form').reset();
+
   document.getElementById('my-form').addEventListener('submit', function(event) {
   event.preventDefault(); 
-
+    
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
 
-  localStorage.setItem('userName', name);
-  localStorage.setItem('userEmail', email);
-    
+  const newUser = {
+    name: name,
+    email: email
+  };
+
+  let usersArray = [];
+  const existingUsers = localStorage.getItem('users');
+  if (existingUsers) {
+    usersArray = JSON.parse(existingUsers);
+  }
+
+  // Add the new user to the array
+  usersArray.push(newUser);
+
+  // Store the updated user array in local storage
+  localStorage.setItem('users', JSON.stringify(usersArray));
+
+  // Clear the form
   document.getElementById('my-form').reset();
