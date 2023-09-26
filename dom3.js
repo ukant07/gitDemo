dcb3e836a8387ab5444fd1105eb7e43ffc5bf645
ukant07 <43910600+ukant07@users.noreply.github.com>
2023-09-26 12:29:46 +0530

    dom3.js updated
    Dom manipulation using parentNode  parentElement  childNode &  children

--- dom3.js
+++ dom3.js
@@ -1,1 +1,15 @@
+var itemList = document.querySelector('#items');
+
+//parentNode
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode);
+
+//parentElement
+itemList.parentElement.parentElement.style.backgroundColor = 'red';
+
+//childNode --> gives NodeList
+//console.log(itemList.childNodes);
+
+//children --> gives HTML collection
+itemList.children[1].style.backgroundColor = 'yellow';
 
